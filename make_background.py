4ebac3c6def468c2dae4d1aab48bd93791a3579a
Leonardo Uieda <leouieda@gmail.com>
2017-11-30 17:54:33 -1000

    Script to make the background and some tweaks to poster
    Height can be max 42in because of printer.

---
+++ make_background.py
@@ -0,0 +1,6 @@
+import gmt
+
+fig = gmt.Figure()
+fig.pscoast(region='-270/90/-70/70', projection='M10i', land='#dddddd',
+            water='white', resolution='l')
+fig.savefig('poster_background.png', dpi=1000, transparent=True, Qg=2)
