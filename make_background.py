e61e9c35c424bdfd41771b4175da227178d5c0ba
Leonardo Uieda <leouieda@gmail.com>
2017-12-04 02:06:17 -1000

    Add quakes to bg, use notebook, finish code
    Added the license and sharing as well.

--- make_background.py
+++
@@ -1,6 +0,0 @@
-import gmt
-
-fig = gmt.Figure()
-fig.pscoast(region='-270/90/-70/70', projection='M10i', land='#dddddd',
-            water='white', resolution='l')
-fig.savefig('poster_background.png', dpi=1000)
